5071be9acf429dd5bfa471ca240862c7a8860f8a
Ian Atha <ian@atha.io>
2023-10-30 11:46:50 +0200

    refactor: refactor more of PuffinBasic to Kotlin

---
+++ app/src/main/java/org/puffinbasic/runtime/PrintBuffer.kt
@@ -0,0 +1,38 @@
+package org.puffinbasic.runtime
+
+import it.unimi.dsi.fastutil.bytes.ByteArrayList
+import it.unimi.dsi.fastutil.bytes.ByteList
+import org.puffinbasic.file.PuffinBasicFile
+
+class PrintBuffer {
+    private val buffer: ByteList
+    private var cursor = 0
+
+    init {
+        buffer = ByteArrayList()
+    }
+
+    fun appendAtCursor(value: String) {
+        for (i in buffer.size until cursor + value.length) {
+            buffer.add(SPACE)
+        }
+        for (i in 0 until value.length) {
+            buffer[cursor++] = value[i].code.toByte()
+        }
+    }
+
+    fun flush(file: PuffinBasicFile) {
+        for (i in buffer.indices) {
+            file.writeByte(buffer.getByte(i))
+        }
+        for (i in buffer.indices) {
+            buffer[i] = SPACE
+        }
+        buffer.clear()
+        cursor = 0
+    }
+
+    companion object {
+        private const val SPACE = ' '.code.toByte()
+    }
+}
