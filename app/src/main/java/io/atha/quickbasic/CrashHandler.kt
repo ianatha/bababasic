41fec394d9264a4cdee47062cb23d992b95ce36e
Ian Atha <ian@atha.io>
2023-11-03 21:34:43 +0200

    fix: exclude dev crashes from Crashlytics
    fixes #30

--- app/src/main/java/io/atha/quickbasic/CrashHandler.kt
+++ app/src/main/java/io/atha/quickbasic/CrashHandler.kt
@@ -2,6 +2,7 @@ package io.atha.quickbasic
 
 import android.annotation.SuppressLint
 import android.content.Context
+import android.content.pm.ApplicationInfo
 import android.os.Handler
 import android.os.Looper
 import android.widget.Toast
@@ -11,14 +12,19 @@ import com.google.firebase.ktx.Firebase
 class CrashHandler private constructor() : Thread.UncaughtExceptionHandler {
     private val handler = Handler(Looper.getMainLooper())
     private lateinit var context: Context
+    private var isDebugBuild: Boolean = false
 
     fun init(context: Context) {
         this.context = context.applicationContext
+        val applicationInfo = context.packageManager.getApplicationInfo(context.packageName, 0)
+        isDebugBuild = 0 != applicationInfo.flags and ApplicationInfo.FLAG_DEBUGGABLE
         Thread.setDefaultUncaughtExceptionHandler(this)
     }
 
     override fun uncaughtException(thread: Thread, ex: Throwable) {
-        Firebase.crashlytics.recordException(ex)
+        if (!isDebugBuild) {
+            Firebase.crashlytics.recordException(ex)
+        }
 
         handler.post {
             Toast.makeText(
