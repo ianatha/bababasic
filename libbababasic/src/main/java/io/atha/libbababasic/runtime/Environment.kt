dcecff8b5556c16c1650acc5f3206b75d16d2bed
Ian Atha <ian@atha.io>
2023-11-30 15:45:38 +0200

    feat: support file operations
    fixes #50

--- libbababasic/src/main/java/io/atha/libbababasic/runtime/Environment.kt
+++ libbababasic/src/main/java/io/atha/libbababasic/runtime/Environment.kt
@@ -3,5 +3,6 @@ package io.atha.libbababasic.runtime
 interface Environment {
     operator fun get(key: String): String
     operator fun set(key: String, value: String)
+    fun getStorageFolder(): String
 }
 
